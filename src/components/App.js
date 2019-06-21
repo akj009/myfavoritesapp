a0cbb05f6ba12b08aff8960dd0321087fdee1da4
akj009 <develop.abhij03@gmail.com>
2019-06-21 17:36:08 +0530

    Added: Favlist component to pass tests

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React, {Component} from 'react';
+import FavList from "./FavList";
 
 class App extends Component {
 
     render() {
-        return <div />;
+        return <div>
+            <FavList/>
+        </div>;
     }
 }
 
---
+++ src/components/FavList/index.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class FavList extends Component {
+    render() {
+        return <div/>
+    }
+}
+
+export default FavList;
